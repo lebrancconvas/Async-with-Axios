68ad1ea9170d23cc99af95af765cfb2768375fd4
lebrancconvas <lebrancconvas@gmail.com>
2021-12-27 18:56:49 +0700

    🔨 Add Error Catching in getDittoCallback and 💡  Add Comment above getDittoNoHandle and getDittoCallback.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const Axios = require('axios');
 
+//Can we get API Data if we didn't handle anything to this asynchronous GET Function?  
 const getDittoNoHandle = () => {
     const response = Axios.get('https://pokeapi.co/api/v2/pokemon/ditto');
     console.log({ data: response.data, status: 'No Asynchronous Handle Success' });
 }
 
+// Get API Data by Callback Function Way. 
 const getDittoCallback = () => {
-    Axios.get('https://pokeapi.co/api/v2/pokemon/ditto', response => {
-        console.log({ data: response.data, status: 'Callback Success.' });
-    })
+    try {
+        Axios.get('https://pokeapi.co/api/v2/pokemon/ditto', response => {
+            console.log({ data: response.data, status: 'Callback Success.' });
+        })
+    } catch (err) {
+        console.log({ data: err, status: 'Callback Failed.' });
+    }
 }
 
 // Get API Data by Promise Way. 
