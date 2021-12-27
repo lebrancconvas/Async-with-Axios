c8b0113105384c5efe323093e5b5caa027562e84
lebrancconvas <lebrancconvas@gmail.com>
2021-12-27 18:23:33 +0700

    💡 Add Code Comment.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const Axios = require('axios');
 
+// Get API Data by Promise Way. 
 const getDittoPromise = () => {
     Axios.get('https://pokeapi.co/api/v2/pokemon/ditto').then(response => {
         console.log(response.data);
     })
 }
 
+// Get API Data by Async-Await Way.  
 const getDittoAsyncAwait = async() => {
     const response = await Axios.get('https://pokeapi.co/api/v2/pokemon/ditto');
     console.log(response.data);
 }
 
-getDittoPromise();
-getDittoAsyncAwait();
+getDittoPromise(); // Running Success.
+getDittoAsyncAwait(); // Running Success.
