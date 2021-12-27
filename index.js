2d4019d5d9dab1a3bd93da9fe7e2bb46d1d4b4be
lebrancconvas <lebrancconvas@gmail.com>
2021-12-27 18:27:44 +0700

    🍻 Add Semicolon.

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const Axios = require('axios');
 const getDittoPromise = () => {
     Axios.get('https://pokeapi.co/api/v2/pokemon/ditto').then(response => {
         console.log(response.data);
-    })
-}
+    });
+};
 
 // Get API Data by Async-Await Way.  
 const getDittoAsyncAwait = async() => {
     const response = await Axios.get('https://pokeapi.co/api/v2/pokemon/ditto');
     console.log(response.data);
-}
+};
 
 getDittoPromise(); // Running Success.
 getDittoAsyncAwait(); // Running Success.
